f2a6ccd5031d9bb0f7068924bf542b902278b6bc
mishika2004 <mishikamendiratta@gmail.com>
2025-12-08 08:16:16 +0530

    export app for vercel

--- index.js
+++ index.js
@@ -57,8 +57,9 @@ app.get("/users/:id", (req, res) => {
     res.status(404).json({ message: "User not found." });
   }
 });
+module.exports = app;
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
